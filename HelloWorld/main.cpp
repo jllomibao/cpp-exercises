ae3ef15b1f71f61201fc2ddb07c7e14965508359
Jeffrey L. Lomibao <jeffrey.lomibao@georgfischer.com>
2023-01-21 08:40:59 -0800

    Add more code to try code stepping

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+    cout << "Enter your favorite number : ";
+    int num{100};
+    cin >> num;
+    cout << "Your favorite number is " << num << endl;
+    cout << "Have a nice day!" << endl;
     return 0;
 }
